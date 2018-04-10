f2583e16d48574d8ec7c9ed267a490fc5829464e
DiamonCheng <dcphone33@live.com>
2018-04-10 21:46:10 +0800

    tool create

---
+++ bin/start.bat
@@ -0,0 +1,11 @@
+rem
+rem
+rem   Notification:  use JDK 1.8    /   set JAVA PATH
+rem
+rem
+
+java -jar eureka-1.0.jar
+
+rem using customer config
+
+rem java -jar eureka-1.0.jar -Dspring.config.location=out-config.properties
